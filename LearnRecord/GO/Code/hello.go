0e4bcb4e8d54e34ba63078284ae9fd048f150ea3
岳琛 <yuechen@100tal.com>
2018-08-13 13:57:48 +0800

    no message

--- LearnRecord/GO/Code/hello.go
+++ LearnRecord/GO/Code/hello.go
@@ -3,26 +3,21 @@ package main
 import "fmt"
 
 func main() {
-   fmt.Printf("Hello, World!\n")
-   fmt.Printf("w3cschool")
-   //123123
-   /*
-123123
-   */
-	
-	var age int;
-	var b bool = true;
+	fmt.Printf("Hello, World!\n")
+	fmt.Printf("w3cschool")
 
+	// var age int;
+	// var b bool = true;
 
-	var v_name1 v_type
-	v_name1 = value
+	// var v_name1 v_type
+	// v_name1 = value
 
-	var v_name2 = value
+	// var v_name2 = value
 
-	v_name3 := value
+	// v_name3 := value
 
-	var a int = 10
-	var b = 10
-	c := 10
-	
+	// var a int = 10
+	// var b = 10
+	// c := 10
+
 }
---
+++ LearnRecord/GO/Code/spiderGoWeb.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+// import "github.com/PuerkitoBio/goquery"
+
+// import (
+// 	"fmt"
+// 	"github.com/PuerkitoBio/goquery"
+// 	// "log"
+// 	// "net/http"
+// 	// "os"
+// 	// "strconv"
+// )
+
+const (
+	baseUrl string = "https://studygolang.com/topics?p="
+)
+
+func main() {
+
+	// var page int = 1
+	// var count int = getPageCount()
+
+	fmt.Printf("pagepagepage\n")
+
+}
